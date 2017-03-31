6e4e5791064085405ac8f375e7e70d8f2f9c3b14
Carol <akinyicarol66@gmail.com>
2017-03-31 14:46:06 +0300

    Convert RGB image to grayscale

--- app/src/main/java/com/example/hp/first/MainActivity.java
+++ app/src/main/java/com/example/hp/first/MainActivity.java
@@ -1,74 +1,169 @@
 package com.example.hp.first;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.widget.AppCompatSeekBar;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.SeekBar;
+import android.widget.Toast;
 
-import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
+import java.io.FileNotFoundException;
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
+    private static final int RQS_OPEN = 1;
+    Button buttonOpen;
+    ImageView imageView;
 
-    static {
-        if(!OpenCVLoader.initDebug()){
-            Log.d(TAG, "OpenCV not loaded");
-        } else {
-            Log.d(TAG, "OpenCV loaded");
-        }
-    }
-
-    private ImageView iv_image;
-    private AppCompatSeekBar sb_brightness;
-    private Bitmap image;
+    Bitmap bmNormal, bmGrayScale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initViews();
+        buttonOpen = (Button) findViewById(R.id.opendocument);
+        buttonOpen.setOnClickListener(buttonOpenOnClickListener);
+
+        imageView = (ImageView)findViewById(R.id.image);
+        imageView.setOnTouchListener(imageViewOnTouchListener);
     }
 
-    private void initViews(){
-        iv_image = (ImageView)findViewById(R.id.iv_image);
-        sb_brightness = (AppCompatSeekBar)findViewById(R.id.sb_brightness);
-        image = BitmapFactory.decodeResource(getResources(),R.drawable.learn2crack);
-        iv_image.setImageBitmap(image);
-        sb_brightness.setOnSeekBarChangeListener(this);
-    }
+    View.OnTouchListener imageViewOnTouchListener = new View.OnTouchListener(){
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
 
-    private Bitmap increaseBrightness(Bitmap bitmap, int value){
+            if(event.getAction() == MotionEvent.ACTION_DOWN){
+                //user touch on ImageView
+                if(bmNormal != null){
+                    imageView.setImageBitmap(bmNormal);
+                }
+            }else if(event.getAction() == MotionEvent.ACTION_UP){
+                //user release touch on ImageView
+                if(bmGrayScale != null){
+                    imageView.setImageBitmap(bmGrayScale);
+                }
+            }
+            return true;
+        }
+    };
 
-        Mat src = new Mat(bitmap.getHeight(),bitmap.getWidth(), CvType.CV_8UC1);
-        Utils.bitmapToMat(bitmap,src);
-        src.convertTo(src,-1,1,value);
-        Bitmap result = Bitmap.createBitmap(src.cols(),src.rows(), Bitmap.Config.ARGB_8888);
-        Utils.matToBitmap(src,result);
-        return result;
-    }
+    View.OnClickListener buttonOpenOnClickListener =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, RQS_OPEN);
+                }
+            };
 
     @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == RQS_OPEN) {
+            Uri dataUri = data.getData();
+            int w = imageView.getWidth();
+            int h = imageView.getHeight();
+            Toast.makeText(MainActivity.this,
+                    dataUri.toString() + "\n" +
+                            w + " : " + h,
+                    Toast.LENGTH_LONG).show();
 
-        Bitmap edited = increaseBrightness(image,progress);
-        iv_image.setImageBitmap(edited);
-
+            try {
+                bmNormal = bmGrayScale = null;
+                bmNormal = loadScaledBitmap(dataUri, w, h);
+                bmGrayScale = getGrayscale(bmNormal);
+                imageView.setImageBitmap(bmGrayScale);
+                Toast.makeText(MainActivity.this,
+                        bmGrayScale.getWidth() + " x " + bmGrayScale.getHeight(),
+                        Toast.LENGTH_SHORT).show();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
+    private Bitmap getGrayscale(Bitmap src){
 
+        //Custom color matrix to convert to GrayScale
+        float[] matrix = new float[]{
+                0.3f, 0.59f, 0.11f, 0, 0,
+                0.3f, 0.59f, 0.11f, 0, 0,
+                0.3f, 0.59f, 0.11f, 0, 0,
+                0, 0, 0, 1, 0,};
+
+        Bitmap dest = Bitmap.createBitmap(
+                src.getWidth(),
+                src.getHeight(),
+                src.getConfig());
+
+        Canvas canvas = new Canvas(dest);
+        Paint paint = new Paint();
+        ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
+        paint.setColorFilter(filter);
+        canvas.drawBitmap(src, 0, 0, paint);
+
+        return dest;
     }
 
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
+    /*
+    reference:
+    Load scaled bitmap
+    http://android-er.blogspot.com/2013/08/load-scaled-bitmap.html
+     */
+    private Bitmap loadScaledBitmap(Uri src, int req_w, int req_h) throws FileNotFoundException {
 
+        Bitmap bm = null;
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(getBaseContext().getContentResolver().openInputStream(src),
+                null, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, req_w, req_h);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        bm = BitmapFactory.decodeStream(
+                getBaseContext().getContentResolver().openInputStream(src), null, options);
+
+        return bm;
+    }
+
+    public int calculateInSampleSize(BitmapFactory.Options options,
+                                     int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate ratios of height and width to requested height and
+            // width
+            final int heightRatio = Math.round((float) height
+                    / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // Choose the smallest ratio as inSampleSize value, this will
+            // guarantee
+            // a final image with both dimensions larger than or equal to the
+            // requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+
+        return inSampleSize;
     }
 }
+
